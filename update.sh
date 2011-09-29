820b85c5178ff167739e44d10897a6e58c3194a5
Pierre Chapuis <catwell@archlinux.us>
2011-09-29 14:10:48 +0000

    modify update script

--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 git pull origin
-cd ruby
+cd cpp
+./bootstrap || exit 1
+./configure || exit 1
+make || exit 1
+sudo make install || exit 1
+cd ../ruby
 rm -f msgpack*.gem
-sh makegem.sh
-sudo gem install msgpack*.gem
+sh makegem.sh || exit 1
+rvmsudo gem install msgpack*.gem || exit 1
