c97a09766e9ad6935bd50b7cce484aac87b68fc3
Pierre Chapuis <catwell@archlinux.us>
2012-07-03 18:49:20 +0200

    update.sh -> pushed/popd

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 git pull origin
-cd cpp
-./bootstrap || exit 1
-./configure || exit 1
-make || exit 1
-sudo make install || exit 1
-cd ../ruby
-rm -f msgpack*.gem
-sh makegem.sh || exit 1
-rvmsudo gem install msgpack*.gem || exit 1
+pushd cpp
+  ./bootstrap || exit 1
+  ./configure || exit 1
+  make || exit 1
+  sudo make install || exit 1
+popd
+pushd ruby
+  rm -f msgpack*.gem
+  sh makegem.sh || exit 1
+  rvmsudo gem install msgpack*.gem || exit 1
+popd
